d91deafe0779c178f336a14839e104b63de4732b
Lesley Lopez <lopez.lesley96@yahoo.com>
2024-08-21 16:45:15 -0700

    - add config file - add api key and alter info plist

---
+++ CryptoCoin/CryptoCoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCoinApp.swift
+//  CryptoCoin
+//
+//  Created by Lesley Lopez on 8/21/24.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
